2350a7f21b48fd8e5f21931ca470026bd0a240e2
Gokul-96 <gokulakrishnan.m96@gmail.com>
2024-01-12 12:30:37 +0530

    commit3

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { FaHome, FaBars } from 'react-icons/fa';
-import { IoIosClose } from 'react-icons/io';
 import { useMediaQuery } from 'react-responsive';
 
 function Header() {
