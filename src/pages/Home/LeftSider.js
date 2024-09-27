c04bd35f181ae5090be44756a6f4de79d6fc223b
Gokul-96 <gokulakrishnan.m96@gmail.com>
2024-09-27 18:52:03 +0530

    270924

--- src/pages/Home/LeftSider.js
+++ src/pages/Home/LeftSider.js
@@ -5,11 +5,11 @@ function LeftSider() {
     <div className='fixed left-0 bottom-40 px-10 sm:static'>
         <div className='flex flex-col items-center'>
             <div className='flex flex-col gap-3 sm:flex-row'>
-            <a href="https://www.linkedin.com/in/gokulakrishnan-m96/" target="_blank">{" "} <i class="ri-linkedin-fill text-gray-400 text-xl">  </i></a>
+            <a href="https://www.linkedin.com/in/gokulakrishnan-m96/" target="_blank" rel="noopener noreferrer">{" "} <i class="ri-linkedin-fill text-gray-400 text-xl">  </i></a>
 
         
              
-            <a href="https://github.com/Gokul-96" target="_blank"><i class="ri-github-fill text-gray-400 text-xl">
+            <a href="https://github.com/Gokul-96" target="_blank" rel="noopener noreferrer"><i class="ri-github-fill text-gray-400 text-xl">
        
        </i></a>
         
